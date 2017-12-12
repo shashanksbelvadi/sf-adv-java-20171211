c39d8cd907c94849a9a62b246ad9b75be5ae1d46
SimonHGR <simon@dancingcloudservices.com>
2017-12-12 15:50:00 -0800

    Initial Stream exercise "solutions"

--- src/main/java/superiterable/TestSuperIterable.java
+++ src/main/java/superiterable/TestSuperIterable.java
@@ -3,17 +3,20 @@ package superiterable;
 import students.Student;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class TestSuperIterable {
   public static void main(String[] args) {
-    SuperIterable<Student> roster = new SuperIterable<>(Arrays.asList(
+    List<Student> listRoster = Arrays.asList(
         Student.ofNameGradeCourses("Fred", 3.7F, "Math", "Physics"),
         Student.ofNameGradeCourses("Freddy", 3.6F, "Math", "Physics"),
         Student.ofNameGradeCourses("Frederick", 2.6F, "Math", "Physics"),
         Student.ofNameGradeCourses("Freda", 1.0F, "Math", "Physics"),
         Student.ofNameGradeCourses("Jim", 2.7F, "Art"),
         Student.ofNameGradeCourses("Sheila", 3.9F,
-            "Math", "Physics", "Astrophysics")));
+            "Math", "Physics", "Astrophysics"));
+
+    SuperIterable<Student> roster = new SuperIterable<>(listRoster);
 
     roster.forEach(x -> System.out.println(x));
     System.out.println("----------------------");
@@ -50,6 +53,66 @@ public class TestSuperIterable {
             x.getCourses()).map(
                 y -> "Student " + x.getName() + " takes " + y))
         .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    System.out.println(" ----------------- Stream Versions --------------------");
+
+    listRoster.stream().forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .map(s -> s.getName())
+        .map(s -> s.toUpperCase())
+        .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .map(x -> x.getCourses()) // probably not what we meant! See below...
+        .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .flatMap(x -> x.getCourses().stream()) // loses boundaries!!!
+        .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .flatMap(x -> x.getCourses().stream())
+        .distinct()
+        .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .flatMap(x -> x.getCourses().stream().map(
+            y -> "Student " + x.getName() + " takes " + y))
+        .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .map(s -> s.getName())
+        .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .flatMap(x -> x.getCourses().stream())
+        .distinct()
+        .map(x -> x.toUpperCase())
+        .forEach(x -> System.out.println(x));
+    System.out.println("----------------------");
+
+    long count = listRoster.stream()
+        .filter(s -> s.getGpa() > 2.9F)
+        .flatMap(x -> x.getCourses().stream())
+        .distinct()
+        .count();
+        System.out.println("There are " + count + " distinct courses being taken by students");
     System.out.println("----------------------");
 
   }
